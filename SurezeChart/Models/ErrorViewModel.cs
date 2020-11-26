2c6ba98acfd7a8a53e0ddeb37525de8f1e95e042
ejazmusavi <ejazmusavi@live.com>
2020-11-26 13:04:31 +0500

    Add project files.

---
+++ SurezeChart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SurezeChart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

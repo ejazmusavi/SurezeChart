7f833a01a728fa979259e0a453b86e9f186bd825
ejazmusavi <ejazmusavi@live.com>
2021-03-22 13:37:43 +0500

    changes for security and identity

--- SurezeChart/Hubs/ChartHub.cs
+++ SurezeChart/Hubs/ChartHub.cs
@@ -9,9 +9,15 @@ using System.Threading.Tasks;
 
 namespace SurezeChart.Hubs
 {
+    public class ChartTime
+    {
+        public int StripId { get; set; }
+        public DateTime LastTime { get; set; }
+    }
     public class ChartHub : Hub
     {
         ApplicationDbContext _context;
+        public static List<ChartTime> LastTime = new List<ChartTime>();
         //static List<stripdatalog2> dummyData = null;
 
         public ChartHub(ApplicationDbContext context)
@@ -20,23 +26,49 @@ namespace SurezeChart.Hubs
          
         }
 
-        public static DateTime dt = DateTime.Now;
-
-
         static int index = 0;
         public void GetData()
         {
-            var data = _context.StripData.AsNoTracking().Where(w => w.StripId == 58).OrderBy(o => o.CreatedDt).Skip(index).Take(1).Select(s => new ChartUpdateData { FHR1 = s.FHR1, FHR2 = s.FHR2, TOCO = s.TOCO1, Date = dt }).FirstOrDefault();
+            var lasttime = LastTime.Where(w => w.StripId == 58).Select(s=>s.LastTime).FirstOrDefault();
+            var ctg = _context.StripData.AsNoTracking().
+                Where(w => w.StripId == 58 & w.CreatedDt >= lasttime).
+                OrderBy(o => o.CreatedDt).Take(2).
+                Select(s => new ChartUpdateData { FHR1 = s.FHR1, FHR2 = s.FHR2, TOCO = s.TOCO1, Date = s.CreatedDt.Value }).ToList();
 
-            if (index%10==0)
+            for (int i = 0; i < ctg.Count(); i++)
             {
-                data.FHR1 = null;
+                if (i == 0)
+                    continue;
+                int j = i;
+                LoopAgain:
+                double seconds2 = (ctg[i].Date - (ctg[i - 1].Date)).TotalSeconds;
+                if (seconds2 > 1)
+                {
+                    var data = new ChartUpdateData() { };
+                    data.Date = ctg[i - 1].Date.AddSeconds(1);
+                    data.FHR1 = null;
+                    data.FHR2 = null;
+                    data.TOCO = null;
+                    data.Notes = null;
+                    ctg.Insert(i, data);
+                    goto LoopAgain;
+                }
             }
-            if (data != null)
-                index += 1;
-            else index = 0;
-            dt = dt.AddSeconds(1);
-            Clients.All.SendAsync("updatechart", data);
+
+            var t = ctg.Where(w => w.Date == lasttime).FirstOrDefault();
+            if(t!=null)
+            {
+                ctg.Remove(t);
+            }
+            var lasttime1 = LastTime.Where(w => w.StripId == 58).FirstOrDefault();
+            if (lasttime1 != null)
+                LastTime.Remove(lasttime1);
+
+            ctg = ctg.OrderBy(d => d.Date).ToList();
+
+            LastTime.Add(new ChartTime { StripId = 58, LastTime = ctg.Max(s => s.Date)});
+
+            Clients.All.SendAsync("updatechart", ctg);
         }
 
 
@@ -44,7 +76,7 @@ namespace SurezeChart.Hubs
         {
             var ctg = _context.StripData.AsNoTracking().Where(w => w.StripId == 58).
                 Select(s => new ChartUpdateData { FHR1 = s.FHR1, FHR2 = s.FHR2, TOCO = s.TOCO1, Date = s.CreatedDt.Value }).
-                OrderBy(o => o.Date).Take(1500).ToList();
+                OrderBy(o => o.Date).Take(2000).ToList();
             index = ctg.Count;
             for (int i = 0; i < ctg.Count(); i++)
             {
@@ -65,8 +97,14 @@ namespace SurezeChart.Hubs
                     goto LoopAgain;
                 }
             }
+            var lasttime = LastTime.Where(w => w.StripId == 58).FirstOrDefault();
+            if (lasttime != null)
+                LastTime.Remove(lasttime);
+
             ctg = ctg.OrderBy(d => d.Date).ToList();
-            Clients.Caller.SendAsync("initialChartData", ctg);
+
+            LastTime.Add(new ChartTime { StripId = 58, LastTime = ctg.Max(s => s.Date) });
+            Clients.Caller.SendAsync("initialChartData", ctg );
         }
 
 

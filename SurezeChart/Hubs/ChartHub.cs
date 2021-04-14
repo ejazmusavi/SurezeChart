a31ecf612543e08a83656ee0ba2b70e51949afd0
ejazmusavi <ejazmusavi@live.com>
2021-04-14 21:27:18 +0500

    new chart

--- SurezeChart/Hubs/ChartHub.cs
+++ SurezeChart/Hubs/ChartHub.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using SurezeChart.Data;
-using SurezeChart.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +10,7 @@ namespace SurezeChart.Hubs
 {
     public class ChartTime
     {
+        public string conId { get; set; }
         public int StripId { get; set; }
         public DateTime LastTime { get; set; }
     }
@@ -19,22 +19,97 @@ namespace SurezeChart.Hubs
         ApplicationDbContext _context;
         public static List<ChartTime> LastTime = new List<ChartTime>();
         //static List<stripdatalog2> dummyData = null;
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        } 
 
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
         public ChartHub(ApplicationDbContext context)
         {
             _context = context;
-         
-        }
-        private readonly int stripid = 59;
-        static int index = 0;
-        public void GetData()
-        {
-            var lasttime = LastTime.Where(w => w.StripId == stripid).Select(s=>s.LastTime).FirstOrDefault();
-            var ctg = _context.StripData.AsNoTracking().
-                Where(w => w.StripId == stripid & w.CreatedDt >= lasttime).
-                OrderBy(o => o.CreatedDt).Take(2).
-                Select(s => new ChartUpdateData { FHR1 = s.FHR1, FHR2 = s.FHR2, TOCO = s.TOCO1, Date = s.CreatedDt.Value }).ToList();
 
+        }
+        static int index = 0;
+        public void GetData(int stripid, string conId)
+        {
+            //Clients.All.SendAsync("updatechartError", time.ToString(), "helo");
+            try
+            {
+                var lasttime = LastTime.Where(w => w.StripId == stripid && w.conId==conId).
+                    Select(s => s.LastTime).FirstOrDefault();
+                Clients.All.SendAsync("updatechartError", "time list", LastTime);
+                var ctg = _context.StripData.AsNoTracking().
+                    Where(w => w.StripId == stripid & w.CreatedDt >= lasttime).
+                    OrderBy(o => o.CreatedDt).Take(2).
+                    Select(s => new ChartUpdateData { FHR1 = s.FHR1, FHR2 = s.FHR2, TOCO = s.TOCO1, Date = s.CreatedDt.Value }).ToList();
+
+                for (int i = 0; i < ctg.Count(); i++)
+                {
+                    if (i == 0)
+                        continue;
+
+                    LoopAgain:
+                    double seconds2 = (ctg[i].Date - ctg[i-1].Date).TotalSeconds;
+                    if (seconds2 > 1)
+                    {
+                        var data = new ChartUpdateData() { };
+                        data.Date = ctg[i - 1].Date.AddSeconds(1);
+                        data.FHR1 = null;
+                        data.FHR2 = null;
+                        data.TOCO = null;
+                        data.Notes = null;
+                        ctg.Insert(i, data);
+                        goto LoopAgain;
+                    }
+                    else
+                    {
+                        var fhr1Diff = ctg[i].FHR1 - ctg[i - 1].FHR1;
+
+                        if (fhr1Diff > 15 || fhr1Diff < -15 || ctg[i].FHR1 == 0)
+                        {
+                            ctg[i].FHR1 = null;
+                        }
+
+                        var fhr2Diff = ctg[i].FHR2 - ctg[i - 1].FHR2;
+
+                        if (fhr2Diff > 15 || fhr2Diff < -15 || ctg[i].FHR2 == 0)
+                        {
+                            ctg[i].FHR2 = null;
+                        }
+                    }
+                }
+
+                var t = ctg.Where(w => w.Date == lasttime).FirstOrDefault();
+                if (t != null)
+                {
+                    ctg.Remove(t);
+                }
+                var lasttime1 = LastTime.Where(w => w.StripId == stripid && w.conId==conId).FirstOrDefault();
+                if (lasttime1 != null)
+                    LastTime.Remove(lasttime1);
+
+                ctg = ctg.OrderBy(d => d.Date).ToList();
+                if (ctg.Count > 0)
+                    LastTime.Add(new ChartTime {conId=conId, StripId = stripid, LastTime = ctg.Max(s => s.Date) });
+
+                Clients.All.SendAsync("updatechart", ctg);
+            }
+            catch(Exception ee) {
+                Clients.All.SendAsync("updatechartError", stripid, ee.Message);
+
+            }
+        }
+
+        public void InitialData(int stripid, string conId)
+        {
+            var ctg = _context.StripData.AsNoTracking().Where(w => w.StripId == stripid).
+                Select(s => new ChartUpdateData { FHR1 = s.FHR1, FHR2 = s.FHR2, TOCO = s.TOCO1, Date = s.CreatedDt.Value }).
+                OrderBy(o => o.Date).Take(2000).ToList();
+            index = ctg.Count;
             for (int i = 0; i < ctg.Count(); i++)
             {
                 if (i == 0)
@@ -70,75 +145,15 @@ namespace SurezeChart.Hubs
                     }
                 }
             }
-
-            var t = ctg.Where(w => w.Date == lasttime).FirstOrDefault();
-            if(t!=null)
-            {
-                ctg.Remove(t);
-            }
-            var lasttime1 = LastTime.Where(w => w.StripId == stripid).FirstOrDefault();
-            if (lasttime1 != null)
-                LastTime.Remove(lasttime1);
-
-            ctg = ctg.OrderBy(d => d.Date).ToList();
-            if(ctg.Count>0)
-            LastTime.Add(new ChartTime { StripId = stripid, LastTime = ctg.Max(s => s.Date)});
-
-            Clients.All.SendAsync("updatechart", ctg);
-        }
-
-
-        public void InitialData()
-        {
-            var ctg = _context.StripData.AsNoTracking().Where(w => w.StripId == stripid).
-                Select(s => new ChartUpdateData { FHR1 = s.FHR1, FHR2 = s.FHR2, TOCO = s.TOCO1, Date = s.CreatedDt.Value }).
-                OrderBy(o => o.Date).Take(2000).ToList();
-            index = ctg.Count;
-            for (int i = 0; i < ctg.Count(); i++)
-            {
-                if (i == 0)
-                    continue;
-                int j = i;
-            LoopAgain:
-                double seconds2 = (ctg[i].Date - (ctg[i - 1].Date)).TotalSeconds;
-                if (seconds2 > 1)
-                {
-                    var data = new ChartUpdateData() { };
-                    data.Date = ctg[i - 1].Date.AddSeconds(1);
-                    data.FHR1 = null;
-                    data.FHR2 = null;
-                    data.TOCO = null;
-                    data.Notes = null;
-                    ctg.Insert(i, data);
-                    goto LoopAgain;
-                }
-                else
-                {
-                    var fhr1Diff = ctg[i].FHR1 - ctg[i - 1].FHR1;
-
-                    if(fhr1Diff>15 || fhr1Diff<-15 || ctg[i].FHR1 == 0)
-                    {
-                        ctg[i].FHR1 = null;
-                    }
-
-                    var fhr2Diff = ctg[i].FHR2 - ctg[i - 1].FHR2;
-
-                    if (fhr2Diff > 15 || fhr2Diff < -15 || ctg[i].FHR2 == 0)
-                    {
-                        ctg[i].FHR2 = null;
-                    }
-                }
-            }
-            var lasttime = LastTime.Where(w => w.StripId == stripid).FirstOrDefault();
+            var lasttime = LastTime.Where(w => w.StripId == stripid && w.conId==conId).FirstOrDefault();
             if (lasttime != null)
                 LastTime.Remove(lasttime);
 
             ctg = ctg.OrderBy(d => d.Date).ToList();
-            if(ctg.Count>0)
-            LastTime.Add(new ChartTime { StripId = stripid, LastTime = ctg.Max(s => s.Date) });
-            Clients.Caller.SendAsync("initialChartData", ctg );
+            if (ctg.Count > 0)
+                LastTime.Add(new ChartTime { conId = conId, StripId = stripid, LastTime = ctg.Max(s => s.Date) });
+            Clients.Caller.SendAsync("initialChartData", ctg);
         }
-
 
         public class ChartUpdateData
         {
